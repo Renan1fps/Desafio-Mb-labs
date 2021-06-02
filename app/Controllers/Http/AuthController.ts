789849e580c1d5ec91818af39dc5809e30fb5711
Renan Oliveira <renan.sergipe68@gmail.com>
2021-06-02 10:47:12 -0300

    Login e logout da aplicação

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,15 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AuthController {
+  public async store({ request, auth }: HttpContextContract) {
+    const { email, password } = request.all()
+    const token = await auth.attempt(email, password, {
+      expiresIn: '1 days',
+    })
+    return token
+  }
+
+  public async destroy({ auth }: HttpContextContract) {
+    await auth.logout()
+  }
+}
