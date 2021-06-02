ede89400a3f8c69e9582e8bbef0c9527b0091860
Renan Oliveira <renan.sergipe68@gmail.com>
2021-06-02 09:21:18 -0300

    Definindo controller de eventos

--- app/Controllers/Http/EventsController.ts
+++ app/Controllers/Http/EventsController.ts
@@ -1,8 +1,34 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+/* eslint-disable prettier/prettier */
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Event from 'App/Models/Event'
 
 export default class EventsController {
-  // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
-  index() {
-    return ['evento 1', 'evento 2']
+  public async index({}: HttpContextContract) {
+    const events = await Event.all()
+    return events
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(['name', 'description', 'place', 'data'])
+    const event = await Event.create(data)
+    return event
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const event = await Event.findOrFail(params.id)
+    return event
+  }
+
+  public async update({ request, params }: HttpContextContract) {
+    const event = await Event.findOrFail(params.id)
+    const data = request.only(['name', 'description', 'place', 'data'])
+    event.merge(data)
+    await event.save()
+    return event
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const event = await Event.findOrFail(params.id)
+    await event.delete()
   }
 }
