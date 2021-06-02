a226fb02c8e79007978042d9cad546f27a20bb2a
Renan Oliveira <renan.sergipe68@gmail.com>
2021-06-02 08:45:01 -0300

    Setup do projeto, criando migrations e definindo primeira rota

---
+++ app/Controllers/Http/EventsController.ts
@@ -0,0 +1,8 @@
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class EventsController {
+  // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
+  index() {
+    return ['evento 1', 'evento 2']
+  }
+}
---
+++ database/migrations/1622633632630_events.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Events extends BaseSchema {
+  protected tableName = 'events'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name')
+      table.text('description','longtext')
+      table.string('place')
+      table.string('data')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
